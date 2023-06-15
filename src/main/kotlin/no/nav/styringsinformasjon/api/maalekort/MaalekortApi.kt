ab172bbe89294f4ec01fc3c4c29151ee1ec35a71
Tor-Christian Tangenes <torchrct@gmail.com>
2023-06-16 00:05:38 +0200

    Use jackson for serialization

--- src/main/kotlin/no/nav/styringsinformasjon/api/maalekort/MaalekortApi.kt
+++ src/main/kotlin/no/nav/styringsinformasjon/api/maalekort/MaalekortApi.kt
@@ -1,15 +1,11 @@
 package no.nav.styringsinformasjon.api.maalekort
 
-import com.google.gson.Gson
 import io.ktor.server.application.*
-import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import no.nav.styringsinformasjon.persistence.DatabaseInterface
 import no.nav.styringsinformasjon.persistence.deleteMaalekortXmlByUUID
 import no.nav.styringsinformasjon.persistence.fetchEveryMaalekortXml
-
-val gson = Gson()
 
 fun Routing.registerMaalekortApi(
     databaseAccess: DatabaseInterface
@@ -17,9 +13,8 @@ fun Routing.registerMaalekortApi(
     route("/api/v1/maalekort") {
         get {
             val maalekortList = databaseAccess.fetchEveryMaalekortXml()
-            val response = gson.toJson(maalekortList.map { entry -> entry.xml })
             // maalekortList.forEach { databaseAccess.deleteMaalekortXmlByUUID(it.uuid) }
-            call.respond(response)
+            call.respond(maalekortList.map { entry -> entry.xml })
         }
     }
 }
