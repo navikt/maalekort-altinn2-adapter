b88fee84cdbff9e5ef203fa7765ebde597153352
Tor-Christian Tangenes <40393162+torchr89@users.noreply.github.com>
2023-06-16 00:25:32 +0200

    Make endpoint return XML-content of DB (#5)
    * Make endpoint return XML-content of DB

--- src/main/kotlin/no/nav/styringsinformasjon/api/maalekort/MaalekortApi.kt
+++ src/main/kotlin/no/nav/styringsinformasjon/api/maalekort/MaalekortApi.kt
@@ -3,11 +3,18 @@ package no.nav.styringsinformasjon.api.maalekort
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import no.nav.styringsinformasjon.persistence.DatabaseInterface
+import no.nav.styringsinformasjon.persistence.deleteMaalekortXmlByUUID
+import no.nav.styringsinformasjon.persistence.fetchEveryMaalekortXml
 
-fun Routing.registerMaalekortApi() {
+fun Routing.registerMaalekortApi(
+    databaseAccess: DatabaseInterface
+) {
     route("/api/v1/maalekort") {
         get {
-            call.respondText("Kalte endepunktet GET /api/v1/maalekort")
+            val maalekortList = databaseAccess.fetchEveryMaalekortXml()
+            // maalekortList.forEach { databaseAccess.deleteMaalekortXmlByUUID(it.uuid) }
+            call.respond(maalekortList.map { entry -> mapOf( entry.uuid to entry.xml ) })
         }
     }
 }
--- src/main/kotlin/no/nav/styringsinformasjon/persistence/MaalekortQueries.kt
+++ src/main/kotlin/no/nav/styringsinformasjon/persistence/MaalekortQueries.kt
@@ -5,22 +5,50 @@ import java.time.LocalDateTime
 import java.util.*
 
 fun DatabaseInterface.storeMaalekortXml(xml: String) {
-    val insertStatement1 = """INSERT INTO MAALEKORT_XML (
+    val insertStatement = """INSERT INTO MAALEKORT_XML (
         uuid,
         xml,
         opprettet) VALUES (?, ?, ?)
-    """.trimIndent()
+    """.trimMargin()
 
     val now = Timestamp.valueOf(LocalDateTime.now())
     val entryUuid = UUID.randomUUID()
 
     connection.use { connection ->
-        connection.prepareStatement(insertStatement1).use {
+        connection.prepareStatement(insertStatement).use {
             it.setObject(1, entryUuid)
             it.setString(2, xml)
             it.setTimestamp(3, now)
             it.executeUpdate()
         }
+        connection.commit()
+    }
+}
+
+fun DatabaseInterface.fetchEveryMaalekortXml(): List<MaalekortXml> {
+    val selectQuery = """SELECT *
+                         FROM MAALEKORT_XML
+    """.trimMargin()
+
+    return connection.use { connection ->
+        connection.prepareStatement(selectQuery).use {
+            it.executeQuery().toList { toMaalekortXml() }
+        }
+    }
+}
+
+fun DatabaseInterface.deleteMaalekortXmlByUUID(uuid: String) {
+    val updateStatement = """DELETE
+                             FROM MAALEKORT_XML
+                             WHERE uuid = ?
+    """.trimIndent()
+
+    connection.use { connection ->
+        connection.prepareStatement(updateStatement).use {
+            it.setObject(1, UUID.fromString(uuid))
+            it.executeUpdate()
+        }
+
         connection.commit()
     }
 }
